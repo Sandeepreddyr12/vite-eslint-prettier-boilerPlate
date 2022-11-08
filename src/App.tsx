deadc1dec63e19aa18fa13c67483be03b9b3902f
sandeep reddy <sandeepreddyr12@gmail.com>
2022-11-08 22:39:24 +0530

    configured eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       Hi there!
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
